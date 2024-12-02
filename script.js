6fb46da4e257fdb771abe89ffc9fe5f6a8609994
Anurag Pandey <anurag001pandey@gmail.com>
2024-12-02 19:07:36 +0530

    footer, about page done with adding locomotive in app

--- script.js
+++ script.js
@@ -58,6 +58,9 @@ function loadingAnimation() {
       opacity: 0,
     }
   );
+  tl.from("#page1-center-data1,#page2 ", {
+    opacity: 0,
+  },("-=1.2"));
 }
 function cursorAnimation() {
   document.addEventListener("mousemove", (details) => {
@@ -73,5 +76,39 @@ function cursorAnimation() {
   });
 }
 
+function locomotiveScrollTrigger() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    pinType: document.querySelector(".smooth-scroll").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+
 loadingAnimation();
-cursorAnimation();
+// cursorAnimation();
+locomotiveScrollTrigger();
